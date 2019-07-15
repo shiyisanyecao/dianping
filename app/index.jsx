c711a2ccfd802ce769d1ea605053ade2e7569544
shiyisanyecao <2327532812@qq.com>
2019-07-15 10:59:07 +0800

    'config'

--- app/index.jsx
+++ app/index.jsx
@@ -3,16 +3,16 @@ import { render } from 'react-dom'
 
 // import './static/css/common.less'
 
-// class Hello extends React.Component {
-//     render() {
-//         var str = "hello world"
-//         return (
-//             <div>{str} 123 </div>
-//         )
-//     }
-// }
+class Hello extends React.Component {
+    render() {
+        var str = "hello world"
+        return (
+            <div>{str} 123 </div>
+        )
+    }
+}
 
 render(
-    <h1>hahaha</h1>,
+    <Hello />,
     document.getElementById('root')
 )
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const OpenBrowserPlugin = require('open-browser-webpack-plugin');
+const OpenBrowserPlugin = require('open-browser-webpack-plugin');
 
 module.exports = {
   entry: path.resolve(__dirname,'app/index.jsx'),
@@ -15,32 +15,30 @@ module.exports = {
   module: {
     rules: [
         { test: /\.(js|jsx)$/,exclude:/node_modules/,loader:'babel-loader',query:{presets:['react']} },
-        // { test:/\.less$/,exclude:/node_modules/,loader:'style!css!postcss!less' },
-        // { test:/\.css$/,exclude:/node_modules/,loader:'style!css!postcss' },
-        // { test:/\.(png|gif|jpg|jpeg|bmp)$/$,loader:'url-loader!limit=5000' },
-        // { test:/\.(png|woff|woff2|svg|ttf|eot)($|\?)/$,loader:'url-loader!limit=5000' }
+        { test:/\.less$/,exclude:/node_modules/,loader:'style!css!postcss!less' },
+        { test:/\.css$/,exclude:/node_modules/,loader:'style!css!postcss-loader' },
+        { test:/\.(png|gif|jpg|jpeg|bmp)$/,loader:'url-loader' },
+        { test:/\.(png|woff|woff2|svg|ttf|eot)($|\?)/,loader:'url-loader' }
     ]
   },
-//   postcss: [
-//       require('autoprefixer')
-//   ],
   plugins: [
+    require('autoprefixer'),
     new HtmlWebpackPlugin({
       template: __dirname = __dirname+'/app/index.tmpl.html',
       filename: 'index.html'
     }),
-    // new webpack.HotModuleReplacementPlugin(),
-    // new OpenBrowserPlugin({
-    //     url: 'http://localhost:8080'
-    // }),
-    // new webpack.DefinePlugin({
-    //     __DEV__: JSON.stringify(JSON.parse((process.env.NODE_ENV == 'dev') || 'false'))
-    // })
+    new webpack.HotModuleReplacementPlugin(),
+    new OpenBrowserPlugin({
+        url: 'http://localhost:8080'
+    }),
+    new webpack.DefinePlugin({
+        __DEV__: JSON.stringify(JSON.parse((process.env.NODE_ENV == 'dev') || 'false'))
+    })
   ],
-//   devServer: {
-//       colors: true,
-//       historyApiFallback: true,
-//       inline: true,
-//       hot: true
-//   }
+  devServer: {
+      colors: true,
+      historyApiFallback: true,
+      inline: true,
+      hot: true
+  }
 };
