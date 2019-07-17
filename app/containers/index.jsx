2dbdb233df534dd3de69132ce2ab82e2a0392d8b
shiyisanyecao <2327532812@qq.com>
2019-07-17 21:30:07 +0800

    app

--- app/containers/index.jsx
+++ app/containers/index.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import PureRenderMixin from 'react-addons-pure-render-mixin'
-// import LocalStore from '../util/localStore.js'
-// import {CITYNAME} from '../config/localStoreKey.js'
-// import { bindActionCreators } from 'redux'
-// import { connect } from 'react-redux'
-// import * as userInfoActionsFormOtherFile from '../actions/userinfo.js'
+import LocalStore from '../util/localStore.js'
+import {CITYNAME} from '../config/localStoreKey.js'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import * as userInfoActionsFormOtherFile from '../actions/userinfo.js'
 
 class App extends React.Component {
     constructor(props, context) {
         super(props, context);
-        // this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate();
+        this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate();
         this.state = {
             initDone: false
         }
